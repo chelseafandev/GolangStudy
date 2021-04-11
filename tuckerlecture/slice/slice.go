843624c225dc8033a070364300a3ac7ce9360887
junhaeng90 <bluetomorrow90@gmail.com>
2021-04-12 00:34:07 +0900

    slice 추가 강좌

--- tuckerlecture/slice/slice.go
+++ tuckerlecture/slice/slice.go
@@ -30,4 +30,47 @@ func main() {
 	d = append(d, 3)
 	fmt.Printf("address(c):%p\n", &c)
 	fmt.Printf("address(d):%p\n", &d)
+
+	fmt.Println("================================")
+	sliceTest()
+
+	fmt.Println("================================")
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for i := 0; i < 5; i++ {
+		var target int
+		input, target = RemoveBack(input)
+		fmt.Println(target)
+	}
+	fmt.Println(input)
+
+	input2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for i := 0; i < 5; i++ {
+		var target int
+		input2, target = RemoveFront(input2)
+		fmt.Println(target)
+	}
+	fmt.Println(input2)
+}
+
+func sliceTest() {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	b := a[4:8]
+	b[0] = 50
+	b[1] = 60
+	c := a[4:]
+	d := a[:4]
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
+
+// 실제 메모리 상에는 값이 남아있지만 가리키는 포인터만 바뀐다
+func RemoveBack(a []int) ([]int, int) {
+	return a[:len(a)-1], a[len(a)-1]
+}
+
+// 실제 메모리 상에는 값이 남아있지만 가리키는 포인터만 바뀐다
+func RemoveFront(a []int) ([]int, int) {
+	return a[1:], a[0]
 }
