e2f35645da49d0fec38f568bc8d8b82386a8fa97
junhaeng90 <bluetomorrow90@gmail.com>
2021-04-11 18:10:39 +0900

    linked list structure 개선

--- tuckerlecture/linkedlist/linkedlist.go
+++ tuckerlecture/linkedlist/linkedlist.go
@@ -7,58 +7,64 @@ type Node struct {
 	next *Node
 }
 
-func main() {
-
-	var root *Node = &Node{val: 0, next: nil}
-	var tail *Node = root
-
-	for i := 1; i < 10; i++ {
-		tail = AddNode(tail, i)
-	}
-	PrintNodes(root)
-	root, tail = RemoveNode(root, root, tail)
-	PrintNodes(root)
-	root, tail = RemoveNode(root.next.next, root, tail)
-	PrintNodes(root)
-	root, tail = RemoveNode(tail, root, tail)
-	PrintNodes(root)
+type LinkedList struct {
+	root *Node
+	tail *Node
 }
 
-func AddNode(tail *Node, val int) *Node {
-	node := &Node{val: val}
-	tail.next = node
-	return node
-}
-
-func RemoveNode(target *Node, root *Node, tail *Node) (*Node, *Node) {
-	if target == root {
-		root = root.next
-		if root == nil {
-			tail = nil
-		}
-		return root, tail
+func (l *LinkedList) AddNode(val int) {
+	if l.root == nil {
+		l.root = &Node{val: val}
+		l.tail = l.root
+		return
 	}
 
-	prev := root
+	l.tail.next = &Node{val: val}
+	l.tail = l.tail.next
+}
+
+func (l *LinkedList) RemoveNode(target *Node) {
+	if target == l.root {
+		l.root = l.root.next
+		return
+	}
+
+	prev := l.root
 	for prev.next != target {
 		prev = prev.next
 	}
 
-	if target == tail {
+	if target == l.tail {
 		prev.next = nil
-		tail = prev
+		l.tail = prev
 	} else {
-		prev.next = (prev.next).next
+		prev.next = target.next
 	}
 
-	return root, tail
+	target.next = nil
 }
 
-func PrintNodes(root *Node) {
-	node := root
+func (l *LinkedList) PrintNodes() {
+	node := l.root
 	for node.next != nil {
-		fmt.Print(node.val, "->")
+		fmt.Print(node.val, " -> ")
 		node = node.next
 	}
 	fmt.Println(node.val)
+}
+
+func main() {
+	list := &LinkedList{}
+	list.AddNode(0)
+	for i := 1; i < 10; i++ {
+		list.AddNode(i)
+	}
+
+	list.PrintNodes()
+	list.RemoveNode(list.root)
+	list.PrintNodes()
+	list.RemoveNode(list.root.next)
+	list.PrintNodes()
+	list.RemoveNode(list.tail)
+	list.PrintNodes()
 }
