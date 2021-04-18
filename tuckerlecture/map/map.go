9319e5d5a8836e7d963aa6d20f2779876a75c37e
junhaeng90 <bluetomorrow90@gmail.com>
2021-04-19 03:11:01 +0900

    hashmap의 add와 get 추가

--- tuckerlecture/map/map.go
+++ tuckerlecture/map/map.go
@@ -6,6 +6,14 @@ import (
 )
 
 func main() {
-	fmt.Println("abcde =", datastruct.Hash("abcde"))
-	fmt.Println("abcde =", datastruct.Hash("abcde"))
+	m := datastruct.NewMap()
+	m.Add("AAA", "01012345678")
+	m.Add("BBB", "01022223333")
+	m.Add("CCC", "01033334444")
+	m.Add("DDD", "01044445555")
+
+	fmt.Println("AAA =", m.Get("AAA"))
+	fmt.Println("BBB =", m.Get("BBB"))
+	fmt.Println("CCC =", m.Get("CCC"))
+	fmt.Println("FFF =", m.Get("FFF"))
 }
