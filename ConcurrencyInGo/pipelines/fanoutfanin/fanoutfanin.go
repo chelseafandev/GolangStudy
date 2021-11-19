0960ee340d112f02ee839506d168c0d392ddf57a
chelseafandev <chelseafandev@gmail.com>
2021-11-19 14:11:27 +0900

    fan-out fan-in 설명 보강

--- ConcurrencyInGo/pipelines/fanoutfanin/fanoutfanin.go
+++ ConcurrencyInGo/pipelines/fanoutfanin/fanoutfanin.go
@@ -3,8 +3,26 @@ Fan-out
 Multiple functions can read from the same channel until that channel is closed; this is called fan-out.
 This provides a way to distribute work amongst a group of workers to parallelize CPU use and I/O.
 
+┏━━━━━━━━━┓
+┃ worker1 ┃<────────────┐
+┗━━━━━━━━━┛			┌───────┐
+					│channel│
+┏━━━━━━━━━┓			└───────┘
+┃ worker2 ┃<────────────┘
+┗━━━━━━━━━┛
+
 Fan-in
 A function can read from multiple inputs and proceed until all are closed by multiplexing the input channels onto a single channel that's closed when all the inputs are closed. This is called fan-in.
+
+					┌────────┐
+					│channel1│
+					└───┬────┘
+┏━━━━━━━━━━┓			│
+┃  worker  ┃<───────────┤
+┗━━━━━━━━━━┛			│
+					┌───┴────┐
+					│channel2│
+					└────────┘
 */
 
 package fanoutfanin
