1af0335e1318eddf3d9203529fc8c7d2215c8ccf
chelseafandev <chelseafandev@gmail.com>
2022-07-28 11:34:36 +0900

    chore: 샘플 코드 추가 방식 변경 - sample.go 파일을 main 패키지로 하고 해당 파일 내에 별도의 함수를 통해 사용법 가이드

---
+++ UnitTest/sample.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	mycrypto "GolangStudy/UnitTest/crypto"
+)
+
+func crypto_sample() {
+	fmt.Println("Hello `Crypto` World!\n")
+
+	password := "mycryptopassword"
+	hash := mycrypto.HASH_FUNCTION_SHA_512
+	salt := "mycryptosalt"
+	iteration := 1000
+	keysize := mycrypto.AES_128_KEY_SIZE
+	iv := "mycryptoiv"
+
+	mc := mycrypto.NewMyCrypto(password, hash, salt, iteration, keysize, iv)
+
+	//
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type a string which you want to encrypt : ")
+	scanner.Scan()
+	plain_text := scanner.Text()
+
+	//
+	result, encrypted_text := mc.Encrypt_aes_with_pbkdf2(plain_text)
+	if result {
+		fmt.Println("\nencrypt_aes128_with_pbkdf2 success :)")
+		fmt.Println("  plain_text     	  : ", plain_text)
+		fmt.Println("  encrypted(base64) 	  : ", encrypted_text)
+		decoded, _ := base64.StdEncoding.DecodeString(encrypted_text)
+		fmt.Println("  encrypted(hex)    	  : ", hex.EncodeToString(decoded))
+	} else {
+		fmt.Println("encrypt_aes128_with_pbkdf2 fail :(")
+	}
+
+	//
+	result, decrypted_text := mc.Decrypt_aes_with_pbkdf2(encrypted_text)
+	if result {
+		fmt.Println("\ndecrypt_aes128_with_pbkdf2 success :)")
+		fmt.Println("  plain_text     	  : ", plain_text)
+		fmt.Println("  decrypted_text	  : ", decrypted_text)
+	} else {
+		fmt.Println("decrypt_aes128_with_pbkdf2 fail :(")
+	}
+}
+
+func main() {
+	crypto_sample()
+}
